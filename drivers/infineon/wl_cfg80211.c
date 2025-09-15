6d526699b74f79ec62a6a6effafb3b2077a2a12a
Xu Xuehui <xxh@rock-chips.com>
2025-09-15 17:38:37 +0800

    fix compile error
    external/rkwifibt/drivers/infineon/wl_cfg80211.c:15717:2: error: #else without #if
    external/rkwifibt/drivers/infineon/wl_cfg80211.c:15731:2: error: #else without #if
    
    Change-Id: Ie31216dc853bc5af7c2633def7a99fd459f139e2
    Signed-off-by: Xu Xuehui <xxh@rock-chips.com>

--- drivers/infineon/wl_cfg80211.c
+++ drivers/infineon/wl_cfg80211.c
@@ -15712,10 +15712,11 @@ s32 wl_inform_bss(struct bcm_cfg80211 *cfg)
 #ifdef ESCAN_CHANNEL_CACHE
 	reset_roam_cache(cfg);
 #endif /* ESCAN_CHANNEL_CACHE */
-if (IS_ENABLED(CONFIG_PREEMPT_RT))
-	migrate_disable();
-#else
-	preempt_disable();
+	if (IS_ENABLED(CONFIG_PREEMPT_RT))
+		migrate_disable();
+	else
+		preempt_disable();
+
 	bi = next_bss(bss_list, bi);
 	for_each_bss(bss_list, bi, i) {
 #ifdef ESCAN_CHANNEL_CACHE
@@ -15726,10 +15727,10 @@ if (IS_ENABLED(CONFIG_PREEMPT_RT))
 			WL_ERR(("bss inform failed\n"));
 		}
 	}
-if (IS_ENABLED(CONFIG_PREEMPT_RT))
-	migrate_enable();
-#else
-	preempt_enable();
+	if (IS_ENABLED(CONFIG_PREEMPT_RT))
+		migrate_enable();
+	else
+		preempt_enable();
 	WL_MEM(("cfg80211 scan cache updated\n"));
 #ifdef ROAM_CHANNEL_CACHE
 	/* print_roam_cache(); */
